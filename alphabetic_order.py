7d4234030ee5d9d95180a5de89116d886bfbb738
unknown <Gagandeep.Chawla@IB-SEZ-PC-028.infobeans.in>
2017-11-14 15:11:36 +0530

    Write a Python Program to Sort Words in Alphabetic Order.

---
+++ alphabetic_order.py
@@ -0,0 +1,5 @@
+#Write a Python Program to Sort Words in Alphabetic Order
+def alphabetic_0rder(ch):
+    return sorted(ch)
+letter = raw_input("Enter the letter\n")
+print (alphabetic_0rder(letter))
